8bb616414ada3b840c9e640170aade69d1c1a689
BRINDHA-96 <brindhak23ec@psnacet.edu.in>
2025-07-30 21:16:33 +0530

    addition.java

---
+++ addition.java
@@ -0,0 +1,22 @@
+public class methoddemo {
+int add(int a, int b)  {
+ return (a+b);
+}
+ public static void main(String[] args) {
+ System.out.println("Psna");
+ Methoddemo obj = new Methoddemo();
+System.out.println(obj.add(30,60));
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
