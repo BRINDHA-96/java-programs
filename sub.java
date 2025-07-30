11cc38f3a704119d6c1d085d135705f25a030407
BRINDHA-96 <brindhak23ec@psnacet.edu.in>
2025-07-30 21:32:17 +0530

    sub.java

---
+++ sub.java
@@ -0,0 +1,36 @@
+public class Methoddemo {
+int sub(int a , int b) {
+return (a-b);
+}
+public static void main(String[] args) {
+System.out.println("Psna");
+ Methoddemo obj = new Methoddemo();
+ System.out.println(obj.sub(60,30));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
