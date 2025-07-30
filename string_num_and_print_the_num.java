5857c25e3b22497c9d21bb09c13a58970ffbb8c3
BRINDHA-96 <brindhak23ec@psnacet.edu.in>
2025-07-30 22:10:57 +0530

    string_num_and_print_the_num.java

---
+++ string_num_and_print_the_num.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        String a = "10", b = "20", c = "30";
+        System.out.println(a + b + c);
+
+        Scanner bird = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int d = bird.nextInt();
+        System.out.println("You entered: " + d);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
