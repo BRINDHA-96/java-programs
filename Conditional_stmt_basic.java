9fbfc75ed31dea69d61197b0941bace2920a7512
BRINDHA-96 <brindhak23ec@psnacet.edu.in>
2025-07-30 22:43:16 +0530

    Conditional_stmt_basic.java

--- Conditional_stmt_basic.java
+++ Conditional_stmt_basic.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner cricket = new Scanner(System.in);
         int a = 10, b = 20;
+        System.out.print("Enter The Number for compare: ");
         int c = cricket.nextInt(); // User inputs a number
+       
 
         if (a == c || b == c) {
             System.out.println("True");
