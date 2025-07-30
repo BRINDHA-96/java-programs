63ebcc6a52912f8e6d8f20eeed62f53483e05ed9
BRINDHA-96 <brindhak23ec@psnacet.edu.in>
2025-07-30 22:33:23 +0530

    Conditional_stmt_basic.java

---
+++ Conditional_stmt_basic.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner cricket = new Scanner(System.in);
+        int a = 10, b = 20;
+        int c = cricket.nextInt(); // User inputs a number
+
+        if (a == c || b == c) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
